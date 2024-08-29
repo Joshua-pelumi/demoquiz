27733752e6d9f30c43760e4d38049f71c7f90cea
Joshua-pelumi <joshuaogundairo9@gmail.com>
2024-08-29 01:08:52 +0100

    add functionality to quit and restart quiz

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,6 +5,8 @@ const exit_btn = info_box.querySelector(".buttons .quit");
 const continue_btn = info_box.querySelector(".buttons .restart");
 const quiz_box = document.querySelector(".quiz_box");
 const timeCount = quiz_box.querySelector(".timer .timer_sec");
+const timeLine = quiz_box.querySelector("header .time_line");
+
 
 const option_list = document.querySelector(".option_list");
 
@@ -26,14 +28,42 @@ continue_btn.onclick = () => {
     showQuestions(0);
     quesCounter(1);
     startTimer(15);
+    startTimerLine(0);
 }
 
 let que_count = 0;
 let que_numb = 1;
 let counter;
+let counterLine;
 let timeValue = 15;
+let widthValue = 0;
+let userScore = 0;
 
 const next_btn = quiz_box.querySelector(".next_btn");
+const result_box = document.querySelector(".result_box");
+const restart_quiz = result_box.querySelector(".buttons .restart");
+const quit_quiz = result_box.querySelector(".buttons .quit");
+
+restart_quiz.onclick = () =>{
+    quiz_box.classList.add("activeQuiz");
+    result_box.classList.remove("activeResult");
+    let que_count = 0;
+    let que_numb = 1;
+    let timeValue = 15;
+    let widthValue = 0;
+    let userScore = 0;
+    showQuestions(que_count);
+    quesCounter(que_numb);
+    clearInterval(counter);
+    startTimer(timeValue);
+    clearInterval(counterLine);
+    startTimerLine(widthtimeValue);
+    next_btn.style.display = "none";
+}
+
+quit_quiz.onclick = () =>{
+    window.location.reload();
+}
 
 //If next button is clicked
 next_btn.onclick = () =>{
@@ -44,9 +74,13 @@ next_btn.onclick = () =>{
         quesCounter(que_numb);
         clearInterval(counter);
         startTimer(timeValue);
+        clearInterval(counterLine);
+        startTimerLine(widthtimeValue);
+        next_btn.style.display = "none";
     }
     else{
         console.log("Questions completed");
+        showResultBox();
     }
 }
 
@@ -71,10 +105,15 @@ let tickIcon = '<div class="icon tick"> <i class="fas fa-check"></i></div>';
 let crossIcon = '<div class="icon cross"> <i class="fas fa-times"></i></div>';
 
 function optionSelected(answer){
+    clearInterval(counter);
+    clearInterval(counterLine);
     let userAns = answer.textContent;
     let correctAns = questions[que_count].answer;
     let allOptions = option_list.children.length;
+   
     if(userAns == correctAns){
+        userScore +=1;
+        console.log(userScore);
         answer.classList.add("correct");
         console.log("Answer is correct");
         answer.insertAdjacentHTML("beforeend", tickIcon)
@@ -97,6 +136,7 @@ function optionSelected(answer){
     for (let i = 0; i < allOptions; i++){
         option_list.children[i].classList.add("disabled");
     }
+    next_btn.style.display = "block";
 }
 
 function startTimer(time){
@@ -104,9 +144,47 @@ function startTimer(time){
     function timer(){
         timeCount.textContent = time; 
         time--;
+        if(time < 9){
+            let addZero = timeCount.textContent;
+            timeCount.textContent = "0", addZero;
+        }
+        if(time < 0){
+            clearInterval(counter);
+            timeCount.textContent = "00  ";
+        }
     }
 }
- 
+
+function showResultBox(){
+    info_box.classList.remove("activeInfo"); //hide info box
+    quiz_box.classList.remove("activeQuiz"); // hide quiz box
+    result_box.classList.add("activeResult"); //show result box
+    const scoreText =  result_box.querySelector(".score_text");
+    if(userScore > 3){
+        let scoreTag =  '<span> and congrats🥉, you got <p>' + userScore + '</p> out of <p>' + questions.lenth + '</p> </span>';
+        scoreText.innerHTML = scoreTag;
+    }
+    else if(userScore > 1){
+        let scoreTag =  '<span> and nice, you got only <p>' + userScore + '</p> out of <p>' + questions.lenth + '</p> </span>';
+        scoreText.innerHTML = scoreTag;
+    }
+    else{
+        let scoreTag =  '<span> and Oops, you got <p>' + userScore + '</p> out of <p>' + questions.lenth + '</p> </span>';
+        scoreText.innerHTML = scoreTag;
+    }
+}
+
+function startTimerLine(time){
+    counterLine = setInterval(timer, 29);
+    function timer(){
+        time +=1;
+        timeLine.style.width = time + "px";
+        if(time < 549){
+            clearInterval(counterLine);
+        }
+    }
+}
+
 function quesCounter(index){
     const buttom_ques_counter = quiz_box.querySelector(".total_que");
     let totalQuesCountTag = '<span><p>' + index + '</p> of <p>' + questions.length  + '</p> Questions</span>';
